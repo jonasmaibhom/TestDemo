af65ea3d54a0211277e5b0fb36f54d00e63210b4
Jonas Maibohm Poulsen <jona6321@edu.ucl.dk>
2019-12-17 22:59:30 +0100

    fjernet udkommenteret kode

--- TestDemoIntegrationTest/MoviesControllerIntegrationTest.cs
+++ TestDemoIntegrationTest/MoviesControllerIntegrationTest.cs
@@ -15,27 +15,6 @@ namespace TestDemoIntegrationTest
 {
     public class MoviesControllerIntegrationTest
     {
-        //[Fact]
-        //public void asdasdsadt()
-        //{
-        //    //Arrange
-        //    var movie = new Mock<Movie>();
-        //    var repo = new Mock<IRepository<IEntity>>();
-        //    var controller = new Mock<MoviesController>(repo.Object);
-
-
-
-        //    //Act
-        //    var movies = controller.Object.Get(1);
-
-
-        //    //Assert
-        //    Assert.IsType<OkResult>(movies.Result.Result);
-        //}
-
-
-
-
 
 
         [Fact]
